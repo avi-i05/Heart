784260d8e5af1f6b6e828424bece21c597df4997
avi-i05 <avisharmaaa373@gmail.com>
2025-05-16 22:59:54 +0530

    dashboard

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 import mongoose from 'mongoose';
 
 const patientSchema = new mongoose.Schema({
+=======
+import mongoose from "mongoose";
+
+const patientSchema = new mongoose.Schema({
+  
+>>>>>>> b0d913907118750f2a3eda276a4881900df8c2f9
   age: Number,
   sex: Number,
   cp: Number,
@@ -14,9 +21,16 @@ const patientSchema = new mongoose.Schema({
   slope: Number,
   ca: Number,
   thal: Number,
+<<<<<<< HEAD
   prediction: Number,
 });
 
 const Patient = mongoose.model('Patient', patientSchema);
 
 export default Patient;
+=======
+
+}, { timestamps: true });
+
+export default mongoose.model("Patient", patientSchema);
+>>>>>>> b0d913907118750f2a3eda276a4881900df8c2f9
